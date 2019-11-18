a1d33252f950659ac015541083de47f3d5026e43
vlasikov <vlasikov@yandex.ru>
2019-11-18 17:42:49 +0300

    .

--- PWM_EXAMPLE21/src/example2.c
+++ PWM_EXAMPLE21/src/example2.c
@@ -102,6 +102,14 @@ static void pwm_callback_1 (void)
 	//pwm_set_duty_cycle_percent(&pwm_topA, duty_cycle_percent_topA);
 }
 
+#define PD3 IOPORT_CREATE_PIN(PORTD, 3)
+static uint8_t second = 0;
+static uint16_t tc_clksel_div = TC_CLKSEL_DIV8_gc;
+static uint16_t Top_tc_period = 15000;
+static const uint16_t Top_tc_period_min = 2000;
+static uint8_t MotorPower = 10;
+static uint8_t MotorPowerMax = 25;
+static bool MotorOk = false;
 /**
  * \brief PWM channel 2 interrupt callback function
  */
@@ -114,87 +122,239 @@ static void pwm_callback_2 (void)
 	//duty_cycle_percent_botA = 75;
 	/* Set new duty cycle value */
 	//pwm_set_duty_cycle_percent(&pwm_botA, duty_cycle_percent_botA);
+	
+	switch (step){
+		case 0:
+			if (ac_get_status(&ACA, 0))
+				pwm_set_duty_cycle_percent(&pwm_botB, 0);
+			else
+				pwm_set_duty_cycle_percent(&pwm_botB, MotorPower);
+			break;
+		case 30:
+			if (!ac_get_status(&ACA, 0))
+				pwm_set_duty_cycle_percent(&pwm_botA, 0);
+			else
+				pwm_set_duty_cycle_percent(&pwm_botA, MotorPower);
+			break;
+		case 4:
+			if (!ac_get_status(&ACA, 0))
+				pwm_set_duty_cycle_percent(&pwm_botA, 0);
+			else
+				pwm_set_duty_cycle_percent(&pwm_botA, MotorPower);
+			break;
+		case 50:
+			if (ac_get_status(&ACA, 0))
+				pwm_set_duty_cycle_percent(&pwm_botB, 0);
+			else
+				pwm_set_duty_cycle_percent(&pwm_botB, MotorPower);
+			break;
+		default:
+			break;
+	}
+	
 }
 
-static void timerC1_tick(void){
+/*
+ *freq = 
+ *
+*/
+static void timerC1_tick(void){	
 	if (++step >= 6) {
 		step = 0;
 	}
 	
 	switch (step){
 		case 0:
-			ioport_set_pin_level(PWM_TOPA, 1);
+			if (Top_tc_period > Top_tc_period_min) {
+//				Top_tc_period *= 0.99;
+				tc_write_period(&TCC1, Top_tc_period);
+				
+				if (Top_tc_period < 30000)
+//					MotorPower = 10;
+					MotorPower = 15;
+				if (Top_tc_period < 15000)
+					MotorPower = 20;
+				if (Top_tc_period < 10000)
+					MotorPower = 20;
+				if (Top_tc_period < 5000)
+					MotorPower = 25;
+			}
+			
 			pwm_set_duty_cycle_percent(&pwm_botA, 0);
 		
 			ioport_set_pin_level(PWM_TOPB, 0);
-			pwm_set_duty_cycle_percent(&pwm_botB, 10);
 		
 			ioport_set_pin_level(PWM_TOPC, 0);
 			pwm_set_duty_cycle_percent(&pwm_botC, 0);
+			
+			ioport_set_pin_level(PWM_TOPA, 1);
+			//pwm_set_duty_cycle_percent(&pwm_botB, MotorPower);
 			break;
 		case 1:
-			ioport_set_pin_level(PWM_TOPA, 1);
 			pwm_set_duty_cycle_percent(&pwm_botA, 0);
 			
 			ioport_set_pin_level(PWM_TOPB, 0);
 			pwm_set_duty_cycle_percent(&pwm_botB, 0);
 			
 			ioport_set_pin_level(PWM_TOPC, 0);
-			pwm_set_duty_cycle_percent(&pwm_botC, 10);
+			
+			ioport_set_pin_level(PWM_TOPA, 1);
+			pwm_set_duty_cycle_percent(&pwm_botC, MotorPower);
 			break;
 		case 2:
 			ioport_set_pin_level(PWM_TOPA, 0);
 			pwm_set_duty_cycle_percent(&pwm_botA, 0);
 		
-			ioport_set_pin_level(PWM_TOPB, 1);
 			pwm_set_duty_cycle_percent(&pwm_botB, 0);
 		
 			ioport_set_pin_level(PWM_TOPC, 0);
-			pwm_set_duty_cycle_percent(&pwm_botC, 10);
+			
+			ioport_set_pin_level(PWM_TOPB, 1);
+			//pwm_set_duty_cycle_percent(&pwm_botC, MotorPower);
 			break;
 		case 3:
 			ioport_set_pin_level(PWM_TOPA, 0);
-			pwm_set_duty_cycle_percent(&pwm_botA, 10);//75
 		
-			ioport_set_pin_level(PWM_TOPB, 1);
 			pwm_set_duty_cycle_percent(&pwm_botB, 0);
 		
 			ioport_set_pin_level(PWM_TOPC, 0);
+			
 			pwm_set_duty_cycle_percent(&pwm_botC, 0);
+			
+			ioport_set_pin_level(PWM_TOPB, 1);
+			pwm_set_duty_cycle_percent(&pwm_botA, MotorPower);
 			break;
 		case 4:
 			ioport_set_pin_level(PWM_TOPA, 0);
-			pwm_set_duty_cycle_percent(&pwm_botA, 10);//75
 		
 			ioport_set_pin_level(PWM_TOPB, 0);
 			pwm_set_duty_cycle_percent(&pwm_botB, 0);
 		
-			ioport_set_pin_level(PWM_TOPC, 1);
 			pwm_set_duty_cycle_percent(&pwm_botC, 0);
+			
+			ioport_set_pin_level(PWM_TOPC, 1);
+			// pwm_set_duty_cycle_percent(&pwm_botA, MotorPower);
 			break;
 		case 5:
 			ioport_set_pin_level(PWM_TOPA, 0);
 			pwm_set_duty_cycle_percent(&pwm_botA, 0);
 		
 			ioport_set_pin_level(PWM_TOPB, 0);
-			pwm_set_duty_cycle_percent(&pwm_botB, 10);
 		
-			ioport_set_pin_level(PWM_TOPC, 1);
 			pwm_set_duty_cycle_percent(&pwm_botC, 0);
+			
+			ioport_set_pin_level(PWM_TOPC, 1);
+			pwm_set_duty_cycle_percent(&pwm_botB, MotorPower);
 			break;
 		default:
 		break;
 	}
 }
-void tc_init(void)
+
+
+void timerD1_tick(void){
+	ioport_toggle_pin_level(IOPORT_CREATE_PIN(PORTD, 3));
+	if (second < 100){
+		second++;
+	}
+	
+	switch(second){
+		case 1:
+			MotorPower = 10;
+			tc_clksel_div = TC_CLKSEL_DIV8_gc;
+			break;
+/*		case 2:
+			MotorPower = 15;
+			tc_clksel_div = TC_CLKSEL_DIV4_gc;
+			break;
+		case 3:
+			MotorPower = 20;
+			tc_clksel_div = TC_CLKSEL_DIV2_gc;
+			break;
+		case 4:
+			MotorPower = 25;
+			tc_clksel_div = TC_CLKSEL_DIV1_gc;
+			break;*/
+		case 3:
+			MotorOk = true;
+			pwm_overflow_int_callback(&pwm_botA, pwm_callback_2);
+			break;
+		default:
+		break;
+	}
+	
+	tc_write_clock_source(&TCC1, tc_clksel_div);
+}
+
+void tcc1_init(void)
 {
-	//Initialisation du Timer 0
+	//Initialisation Timer TCC1
 	tc_enable(&TCC1);	
 	tc_set_overflow_interrupt_callback(&TCC1, timerC1_tick); //Cr?ation d'un callback qui sera execut? quand un overflow du timer sera d?clench?.
 	tc_set_wgm(&TCC1, TC_WG_NORMAL);		//Choix du mode du timer0, dans ce cas il comptera jusqu'? sa valeur "TOP" et retombera ? 0
-	tc_write_period(&TCC1, 15000);			//D?finition de la valeur "TOP"	
+	tc_write_period(&TCC1, Top_tc_period);			//D?finition de la valeur "TOP"	
 	tc_set_overflow_interrupt_level(&TCC1, TC_INT_LVL_LO);	//Activation de l'interruption du timer 				
-	tc_write_clock_source(&TCC1, TC_CLKSEL_DIV8_gc);		//Activation de l'horloge du timer 0
+	tc_write_clock_source(&TCC1, tc_clksel_div);		//Activation de l'horloge du timer 0
+}
+
+void tcd1_init(void)
+{
+	//Initialisation Timer TCD1
+	tc_enable(&TCD1);	
+	tc_set_overflow_interrupt_callback(&TCD1, timerD1_tick); //Cr?ation d'un callback qui sera execut? quand un overflow du timer sera d?clench?.
+	tc_set_wgm(&TCD1, TC_WG_NORMAL);		//Choix du mode du timer0, dans ce cas il comptera jusqu'? sa valeur "TOP" et retombera ? 0
+	tc_write_period(&TCD1, 60000);			//D?finition de la valeur "TOP"	
+	tc_set_overflow_interrupt_level(&TCD1, TC_INT_LVL_LO);	//Activation de l'interruption du timer 				
+	tc_write_clock_source(&TCD1, TC_CLKSEL_DIV1024_gc);		//Activation de l'horloge du timer 0
+}
+
+
+/**
+ * \brief Analog comparator window configuration.
+ *
+ * This struct is intentionally just defined, not declared. The example code
+ * will use the helper functions to setup the analog comparator in window mode.
+ */
+static struct ac_config aca_config;
+
+/**
+ * \brief Analog comparator interrupt callback function
+ *
+ * This function is called when an interrupt has occurred on a channel in analog
+ * comparator.
+ *
+ * \param ac Pointer to the analog comparator (AC) base address which caused
+ *           the interrupt
+ * \param channel The analog comparator channel that caused the interrupt
+ * \param status Analog comparator window status given by a \ref ac_status_t
+ *               value
+ */
+static void example_aca_interrupt_callback(AC_t *ac, uint8_t channel,
+		enum ac_status_t status)
+{
+	if (ac_get_status(&ACA, 0))
+		ioport_set_pin_level(IOPORT_CREATE_PIN(PORTA, 3), 1);
+	else
+		ioport_set_pin_level(IOPORT_CREATE_PIN(PORTA, 3), 0);
+	/*
+	 * If trigger was caused by moving into above or below, switch to
+	 * trigger by being inside. If trigger was caused by moving inside the
+	 * window, switch to trigger on outside.
+	 */
+/*
+	if (status != AC_STATUS_INSIDE) {
+		ac_set_interrupt_mode(&aca_config, AC_INT_MODE_INSIDE_WINDOW);
+	} else {
+		ac_set_interrupt_mode(&aca_config, AC_INT_MODE_OUTSIDE_WINDOW);
+	}
+*/
+//	ac_disable(&ACA, 1);
+//	ac_disable(&ACA, 0);
+//	ac_write_config(&ACA, 0, &aca_config);
+//	ac_enable(&ACA, 0);
+//	ac_enable(&ACA, 1);
+
+//	example_ac_update_window_leds(status);
 }
 
 /**
@@ -212,7 +372,11 @@ int main( void )
 	ioport_set_pin_dir(PWM_TOPA, IOPORT_DIR_OUTPUT);
 	ioport_set_pin_dir(PWM_TOPB, IOPORT_DIR_OUTPUT);
 	ioport_set_pin_dir(PWM_TOPC, IOPORT_DIR_OUTPUT);
-	tc_init();
+	tcc1_init();
+	ioport_set_pin_dir(IOPORT_CREATE_PIN(PORTD, 3), IOPORT_DIR_OUTPUT);
+	tcd1_init();
+	
+	ioport_set_pin_dir(IOPORT_CREATE_PIN(PORTA, 3), IOPORT_DIR_OUTPUT);
 
 	/*
 	  Set up PWM channel
@@ -230,7 +394,35 @@ int main( void )
 	pwm_start(&pwm_botA, 0);
 	pwm_start(&pwm_botB, 0);
 	pwm_start(&pwm_botC, 0);
+	
+	ac_set_interrupt_callback(&ACA, example_aca_interrupt_callback);
 
+	/* Setup the analog comparator B in window mode. */
+	ac_set_mode(&aca_config, AC_MODE_SINGLE );
+	// ac_set_voltage_scaler(&aca_config, 11);
+	ac_set_hysteresis(&aca_config, AC_HYSMODE_LARGE_gc);
+	ac_set_negative_reference(&aca_config, AC_MUXNEG_PIN0_gc);
+	ac_set_positive_reference(&aca_config, AC_MUXNEG_PIN5_gc);
+	ac_set_interrupt_mode(&aca_config, AC_INT_MODE_BOTH_EDGES);	// по обоим фронтам
+	ac_set_interrupt_level(&aca_config, AC_INT_LVL_MED);
+	
+	/*
+	 * Write configuration of analog comparator B channel 0, half of window
+	 * configuration.
+	 */
+	ac_write_config(&ACA, 0, &aca_config);
+	
+	ac_set_negative_reference(&aca_config, AC_MUXNEG_PIN1_gc);
+	/*
+	 * Write configuration of analog comparator B channel 1, other half of
+	 * window configuration.
+	 */	
+	ac_write_config(&ACA, 1, &aca_config);
+	
+	/* Enable all the analog comparator channels. */
+	ac_enable(&ACA, 0);
+	ac_enable(&ACA, 1);
+	
 	while(1) {
 		/* Do nothing. Everything is handPWM_TOPC by interrupts. */
 	}
